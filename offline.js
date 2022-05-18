8e3c123c99fd907cb3b75855d2d5c832e3b40c22
2latemc <christoph.fagerer@gmx.ch>
2022-05-18 08:28:31 +0200

    update
    1

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1652855030,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/finish-sheet0.png",
+		"images/2sadad-sheet0.png",
+		"images/car-sheet0.png",
+		"images/start-sheet0.png",
+		"images/02aa0790086f91a-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/51fde640b06bb-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
